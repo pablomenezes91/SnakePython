4f0b2354c8e61ef4166947d6eecd681b5742c756
Pablo Menezes <pablo.menezes91@gmail.com>
2024-12-06 00:14:57 -0300

    placing the main character

--- snake.py
+++ snake.py
@@ -2,15 +2,25 @@ import curses
 import time 
 
 def game_loop(window):
-    window.addstr(f'Aperte alguma tecla:\n')
+    personagem = [10, 15]
+    window.addch(personagem[0], personagem[1], curses.ACS_DIAMOND)
     while True:
         window.timeout(1000)
         char = window.getch()
         window.clear()
-        if char != -1:
-            window.addstr(f'Tecla apertada: {char}\n')
-        else:
-            window.addstr(f"Nenhuma tecla apertada\n")
+
+        match char:
+            case curses.KEY_UP: 
+                personagem[0] -= 1
+            case curses.KEY_LEFT: 
+                personagem[1] -= 1
+            case curses.KEY_DOWN: 
+                personagem[0] += 1
+            case curses.KEY_RIGHT: 
+                personagem[1] += 1
+            case _:
+                pass
+        window.addch(personagem[0], personagem[1], curses.ACS_DIAMOND)
 
 if __name__ == '__main__':
     curses.wrapper(game_loop)
