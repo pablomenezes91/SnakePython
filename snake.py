e4e53657f57319f7a8b066e5f7724ab8ebff0f6a
Pablo Menezes <pablo.menezes91@gmail.com>
2024-12-06 00:39:55 -0300

    functionalizing the code

--- snake.py
+++ snake.py
@@ -1,37 +1,55 @@
 import curses
 import time 
 
-def game_loop(window):
-
-    curses.curs_set(0)
+def draw_screen(window):
+    window.clear()
     window.border(0)
+
+def draw_actor(actor, window):
+    window.addch(actor[0], actor[1], curses.ACS_DIAMOND)
+
+def get_new_direction(window, timeout=1000):
+    window.timeout(timeout)
+    direction = window.getch()
+    if direction in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT,curses.KEY_RIGHT]:
+        return direction
+    return None
+
+def move_actor(actor, direction):
+    match direction:
+        case curses.KEY_UP: 
+            actor[0] -= 1
+        case curses.KEY_LEFT: 
+            actor[1] -= 1
+        case curses.KEY_DOWN: 
+            actor[0] += 1
+        case curses.KEY_RIGHT: 
+            actor[1] += 1
+        case _:
+            pass
+
+def actor_hit_border(actor,window):
     height, width = window.getmaxyx()
+    if (actor[0] <= 0) or (actor[0] >= height-1):
+        return True
+    if (actor[1] <= 0) or (actor[1] >= width-1):
+        return True
+    return False
 
+def game_loop(window):
+    # Setup Inicial
+    curses.curs_set(0)
     personagem = [10, 15]
-    window.addch(personagem[0], personagem[1], curses.ACS_DIAMOND)
+
     while True:
-        window.timeout(1000)
-        char = window.getch()
-        window.clear()
-        window.border(0)
-
-        match char:
-            case curses.KEY_UP: 
-                personagem[0] -= 1
-            case curses.KEY_LEFT: 
-                personagem[1] -= 1
-            case curses.KEY_DOWN: 
-                personagem[0] += 1
-            case curses.KEY_RIGHT: 
-                personagem[1] += 1
-            case _:
-                pass
-
-        if (personagem[0] <= 0) or (personagem[0] >= height-1):
+        draw_screen(window)
+        draw_actor(personagem, window)
+        
+        direction = get_new_direction(window,timeout=1000)
+        if direction is not None:
+            move_actor(personagem, direction)
+        if actor_hit_border(personagem,window):
             return
-        if (personagem[1] <= 0) or (personagem[1] >= width-1):
-            return
-        window.addch(personagem[0], personagem[1], curses.ACS_DIAMOND)
 
 if __name__ == '__main__':
     curses.wrapper(game_loop)
