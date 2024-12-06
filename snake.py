fd7e7ecf103ad77b49c37d07208a5a0788de71b4
Pablo Menezes <pablo.menezes91@gmail.com>
2024-12-06 00:22:21 -0300

    creating borders

--- snake.py
+++ snake.py
@@ -2,12 +2,18 @@ import curses
 import time 
 
 def game_loop(window):
+
+    curses.curs_set(0)
+    window.border(0)
+    height, width = window.getmaxyx()
+
     personagem = [10, 15]
     window.addch(personagem[0], personagem[1], curses.ACS_DIAMOND)
     while True:
         window.timeout(1000)
         char = window.getch()
         window.clear()
+        window.border(0)
 
         match char:
             case curses.KEY_UP: 
@@ -20,7 +26,13 @@ def game_loop(window):
                 personagem[1] += 1
             case _:
                 pass
+
+        if (personagem[0] <= 0) or (personagem[0] >= height-1):
+            return
+        if (personagem[1] <= 0) or (personagem[1] >= width-1):
+            return
         window.addch(personagem[0], personagem[1], curses.ACS_DIAMOND)
 
 if __name__ == '__main__':
     curses.wrapper(game_loop)
+    print('Perdeu!')
